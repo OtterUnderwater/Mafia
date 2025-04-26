4a38a86872844d9f069b1ec477ae66c86772e23d
Otter <130929560+OtterUnderwater@users.noreply.github.com>
2025-04-26 04:00:50 +0300

    feat: add vuetify

---
+++ frontend/mafia-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-container fluid>
+      <v-app-bar>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-title class="app-bar-title">MAFIA</v-app-bar-title>
+
+        <v-spacer/>
+
+        <v-btn>
+          лол
+        </v-btn>
+      </v-app-bar>
+    </v-container>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
